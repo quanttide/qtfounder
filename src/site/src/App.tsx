8350f738821cfb69e181284e46afec74f99a2d5a
Guo Zhang <zhangguo@quanttide.com>
2026-08-14 22:04:09 +0800

    refactor: remove brain (second brain) page and route

--- src/site/src/App.tsx
+++ src/site/src/App.tsx
@@ -3,7 +3,6 @@ import Layout from "./components/Layout";
 import Home from "./pages/Home";
 import Works from "./pages/Works";
 import WorkDetail from "./pages/WorkDetail";
-import Brain from "./pages/Brain";
 
 // 域名根路径部署（founder.quanttide.com，无子路径前缀）
 export default function App() {
@@ -12,7 +11,6 @@ export default function App() {
       <Layout>
         <Routes>
           <Route path="/" element={<Home />} />
-          <Route path="/brain" element={<Brain />} />
           <Route path="/works" element={<Works />} />
           <Route path="/works/fiction/drafts/:slug" element={<WorkDetail />} />
         </Routes>
